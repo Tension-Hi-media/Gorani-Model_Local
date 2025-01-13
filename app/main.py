0e01d96039312af3b82081af1b885f715716974c
aripos1 <ihsjjang@gmail.com>
2025-01-14 00:31:09 +0900

    123

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from app.models.translation import setup_translation_chain
+import logging
 
 app = FastAPI()
 
 class TranslateRequest(BaseModel):
     text: str
 
+class TranslateResponse(BaseModel):
+    answer: str
+
 @app.post("/translate")
 async def translate(request: TranslateRequest):
     # 받은 JSON 데이터 처리
-    translated_text = f"Translated: {request.text}"
-    return {"translated_text": translated_text}
+    try:
+        print(request)
+        chain = setup_translation_chain()
+
+        response = chain.invoke({"text": request.text})
+
+        return TranslateResponse(
+            answer=response
+        )
+    
+    except Exception as e:
+        logging.error(f"Translation error: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
